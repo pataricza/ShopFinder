c3802d8061ff7fc39aaa5fbd307409d12c85aa65
Pataricza György <pataricza1987@gmail.com>
2018-09-02 19:25:59 +0200

    shopcontroller added, index template rendered via controller

---
+++ src/main/java/com/finder/shop/controller/ShopController.java
@@ -0,0 +1,14 @@
+package com.finder.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShopController {
+
+	@GetMapping(value = "/")
+	public String shopIndex() {
+		
+		return "Index";
+	}
+}
