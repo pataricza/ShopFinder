b6a8efded6b33995db40c5805ad1edb28e41dd07
Pataricza György <pataricza1987@gmail.com>
2018-09-09 14:04:25 +0200

    table added to Index template, ShopService layer added

--- src/main/java/com/finder/shop/controller/ShopController.java
+++ src/main/java/com/finder/shop/controller/ShopController.java
@@ -1,14 +1,25 @@
 package com.finder.shop.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.finder.shop.model.Shop;
+import com.finder.shop.service.ShopService;
 
 @Controller
 public class ShopController {
+	
+	@Autowired
+	ShopService shopService;
 
 	@GetMapping(value = "/")
-	public String shopIndex() {
-		
+	public String shopIndex(Model model) {
+		List<Shop> shops = shopService.getAllShops();
+		model.addAttribute("shops", shops);
 		return "Index";
 	}
 	
---
+++ src/main/java/com/finder/shop/service/ShopService.java
@@ -0,0 +1,20 @@
+package com.finder.shop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finder.shop.model.Shop;
+import com.finder.shop.repository.ShopRepository;
+
+@Service
+public class ShopService {
+	
+	@Autowired
+	ShopRepository shopRepository;
+
+	public List<Shop> getAllShops() {
+		return (List<Shop>) shopRepository.findAll();
+	}
+}
