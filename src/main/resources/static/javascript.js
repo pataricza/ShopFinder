6c7397c5ff2123527652203a7b5fece3d61b4148
Pataricza György <pataricza1987@gmail.com>
2018-10-23 23:39:38 +0200

    javascript added, hidden div working

---
+++ src/main/resources/static/javascript.js
@@ -0,0 +1,9 @@
+function details() {
+    var style = $("#details").css("display");
+    
+    if (style === "none") {
+    	$("#details").css("display", "block");
+    } else {
+    	$("#details").css("display", "none");
+    }
+} 
