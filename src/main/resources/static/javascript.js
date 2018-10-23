5b60163d7050d825d569baaef3c9667decc90ab2
Pataricza György <pataricza1987@gmail.com>
2018-10-24 00:04:32 +0200

    button bootstrapped and js refactored

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -1,9 +1,13 @@
-function details() {
+var button = document.getElementById("detailButton");
+
+button.onclick = function() {
     var style = $("#details").css("display");
     
     if (style === "none") {
     	$("#details").css("display", "block");
+    	button.innerHTML = "Rejtsd el!";
     } else {
     	$("#details").css("display", "none");
+    	button.innerHTML = "Mutasd!";
     }
 } 
