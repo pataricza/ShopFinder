31ddc82cca5c868625b68d57c40b9818175a4144
Pataricza György <pataricza1987@gmail.com>
2018-10-25 00:58:51 +0200

    addNewShop dropdown populating with javascript works fine

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -3,8 +3,22 @@ function populateDropDownsWithTime() {
 	var half = ':30';
 	var dropdown = $('.dropdown');
 	
-	for(var i = 0; i < 24; i++) {
-		var time = i+zero;
+	for(var i = 0; i < 48; i++) {
+		var time;
+		
+		if(i % 2 == 0) {
+			if(i < 20) {
+				time = '0' + i/2 + zero;
+			} else {
+				time = i/2 + zero;
+			}
+		} else {
+			if(i < 20) {
+				time = '0' + Math.floor(i/2) +half;
+			} else {
+				time = Math.floor(i/2) + half;
+			}
+		}
 		
 		dropdown.append($('<option></option>')
                 .attr('value', time)
