30b963fee791a67911018ed47d499f53158aecb8
Hidan <pataricza1987@gmail.com>
2019-09-01 01:13:27 +0200

    error message comes from enum

--- src/main/java/com/finder/shop/controller/ShopFinderErrorController.java
+++ src/main/java/com/finder/shop/controller/ShopFinderErrorController.java
@@ -15,8 +15,23 @@ public class ShopFinderErrorController {
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public String handleException(Model model) {
-    model.addAttribute("errorMessage1", "There is no such shop!");
-    model.addAttribute("errorMessage2", "Maybe you want to add it?");
+    model.addAttribute("errorMessage1", ErrorMessage.SHOP_NOT_FOUND_1);
+    model.addAttribute("errorMessage2", ErrorMessage.SHOP_NOT_FOUND_2);
     return "error";
+  }
+  
+  private enum ErrorMessage {
+    SHOP_NOT_FOUND_1("There is no such shop!"),
+    SHOP_NOT_FOUND_2("Maybe you want to add it?");
+    
+    private final String message;
+
+    private ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+      return message;
+    }    
   }
 }
