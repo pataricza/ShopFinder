2cd9dc30ae37164c549f6bdd204a7b44e76be5f2
Hidan <pataricza1987@gmail.com>
2020-02-08 12:42:50 +0100

    file size upload limit added with error handling

--- src/main/java/com/finder/shop/controller/ErrorController.java
+++ src/main/java/com/finder/shop/controller/ErrorController.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 
@@ -14,15 +14,25 @@ public class ErrorController {
 
   @ExceptionHandler(NoSuchElementException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public String handleException(Model model) {
+  public String shopNotFound(Model model) {
     model.addAttribute("errorMessage1", ErrorMessage.SHOP_NOT_FOUND_1);
     model.addAttribute("errorMessage2", ErrorMessage.SHOP_NOT_FOUND_2);
     return "error";
   }
   
+  @ExceptionHandler(MaxUploadSizeExceededException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String fileSizeLimitExceeded(Model model) {
+    model.addAttribute("errorMessage1", ErrorMessage.FILE_SIZE_LIMIT_EXCEEDED_1);
+    model.addAttribute("errorMessage2", ErrorMessage.FILE_SIZE_LIMIT_EXCEEDED_2);
+    return "error";
+  }
+  
   private enum ErrorMessage {
     SHOP_NOT_FOUND_1("There is no such shop!"),
-    SHOP_NOT_FOUND_2("Maybe you want to add it?");
+    SHOP_NOT_FOUND_2("Maybe you want to add it?"),
+    FILE_SIZE_LIMIT_EXCEEDED_1("The picture you have tried to upload is too big."),
+    FILE_SIZE_LIMIT_EXCEEDED_2("Try a picture with a size less than 2MB");
     
     private final String message;
 
--- src/main/java/com/finder/shop/controller/ShopController.java
+++ src/main/java/com/finder/shop/controller/ShopController.java
@@ -3,7 +3,6 @@ package com.finder.shop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.finder.shop.model.Shop;
